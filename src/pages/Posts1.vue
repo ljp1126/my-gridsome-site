2d8ff93ccb088922fd9e86c09577cc968f39f2cc
Labbott <1006762873@qq.com>
2021-01-31 18:33:18 +0800

    GraphQL应用及配置

---
+++ src/pages/Posts1.vue
@@ -0,0 +1,38 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Post 1</h1>
+      <ul>
+        <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Posts1',
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    this.posts = data
+  },
+  components: {
+
+  },
+  methods: {
+
+    direct: function(val) { }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+ 
+</style>
---
+++ src/pages/Posts2.vue
@@ -0,0 +1,48 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Posts2</h1>
+      <ul>
+        <li v-for="edge in $page.posts.edges" :key="edge.node.id">
+          <go-link to='/'>{{ edge.node.title }}</go-link>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+  query {
+    posts: allPost {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+  }
+</page-query>
+
+<script>
+export default {
+  name: 'Posts',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+    direct: function(val) { }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+ 
+</style>
